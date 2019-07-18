508e26c8968ec422b074ca6956ac46fcf0a94288
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-18 14:07:14 -0700

    draft

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,30 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'../views/figures/new'
+  end
+
+  post '/figures/new' do
+    binding.pry
+    @figure = Figure.new(name: params["figure"]["name"])
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'../views/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find#("primary key")
+    @figure.name = params["figure"]["name"]
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  delete '/figures/:id' do
+  
+  end
 end
